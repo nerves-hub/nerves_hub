e53e45253ec7211166484b6517f9c7a85316fc27
Frank Hunleth <fhunleth@troodon-software.com>
2019-01-23 17:37:28 -0500

    WIP - fwup_stream refactor

--- lib/nerves_hub/http_fwup_stream.ex
+++ lib/nerves_hub/http_fwup_stream.ex
@@ -1,16 +1,6 @@
 defmodule NervesHub.HTTPFwupStream do
   @moduledoc """
-  Handles streaming a fwupdate via HTTP.
-
-  a `callback` module is expected to be passed to `start_link/1`
-
-  messages will be received in the shape:
-
-  * `{:fwup_message, message}` - see the docs for
-    [Fwup](https://hexdocs.pm/fwup/) for more info.
-  * `{:http_error, {status_code, body}}`
-  * `{:http_error, :timeout}`
-  * `{:http_error, :too_many_redirects}`
+  Download and install a firmware update.
   """
 
   use GenServer
@@ -20,32 +10,160 @@ defmodule NervesHub.HTTPFwupStream do
   @runtime Application.get_env(:nerves_hub, :runtime)
 
   @redirect_status_codes [301, 302, 303, 307, 308]
+  @httpc_profile __MODULE__
 
-  def start_link(cb) do
-    start_httpc()
-    GenServer.start_link(__MODULE__, [cb])
+  @doc """
+    a `callback` module is expected to be passed to `start_link/1`
+
+  messages will be received in the shape:
+
+  * `{:fwup_message, message}` - see the docs for
+    [Fwup](https://hexdocs.pm/fwup/) for more info.
+  * `{:http_error, {status_code, body}}`
+  * `{:http_error, :timeout}`
+  * `{:http_error, :too_many_redirects}`
+
+  """
+  @spec start_link(keyword()) :: GenServer.on_start()
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  def stop(pid) do
-    GenServer.stop(pid)
+  @doc """
+  Initiate the firmware download from the specified URL
+
+  Optionally send progress reports to the specified pid.
+  """
+  @spec get(String.t(), pid()) :: :ok
+  def get(url, update_pid) do
+    GenServer.call(__MODULE__, {:get, url, update_pid})
   end
 
-  def get(pid, url) do
-    GenServer.call(pid, {:get, url}, :infinity)
+  @doc """
+  Cancel an inprogress firmware update
+  """
+  @spec cancel_update() :: :ok
+  def cancel_update() do
+    GenServer.call(__MODULE__, :cancel_update)
   end
 
-  def init([cb]) do
-    devpath = @runtime.install_device_path()
-    fwup_keys = NervesHub.Certificate.fwup_public_keys()
+  @impl true
+  def init(_opts) do
+    :inets.start(:httpc, profile: @httpc_profile)
 
-    if fwup_keys == [] do
-      Logger.warn(
-        "No firmware signing public keys so not verifying signatures on firmware updates!"
-      )
+    httpc_opts = [
+      max_sessions: 2,
+      max_keep_alive_length: 4,
+      max_pipeline_length: 4,
+      keep_alive_timeout: 120_000,
+      pipeline_timeout: 60_000
+    ]
+
+    :httpc.set_options(httpc_opts, @httpc_profile)
+
+
+    {:ok,
+     %{
+       url: nil,
+       callback: nil,
+       number_of_redirects: 0,
+       timeout: 15000,
+       fwup: nil
+     }}
+  end
+
+  @impl true
+  def handle_call({:get, url, cb}, _from, %{url: nil} = state) do
+    with {:ok, fwup} <- Fwup.stream(cb, fwup_args())
+    make_request(url)
+    # {:ok, fwup} = Fwup.stream(cb, fwup_args())
+    fwup = nil
+
+    {:reply, :ok, %{state | url: url, callback: cb}}
+  end
+
+  @impl true
+  def handle_call({:get, _url, _pid}, _from, state) do
+    {:reply, {:error, "install in progress"}, state}
+  end
+
+  def handle_call(:cancel_update, _from, state) do
+
+  end
+
+  @impl true
+  def handle_info({:http, {_, :stream_start, headers}}, s) do
+    Logger.debug("Stream Start: #{inspect(headers)}")
+
+    {:noreply, s}
+  end
+
+  @impl true
+  def handle_info({:http, {_, :stream, data}}, s) do
+    Logger.debug("Got #{byte_size(data)} bytes")
+    # Fwup.send_chunk(s.fwup, data)
+    {:noreply, s, s.timeout}
+  end
+
+  @impl true
+  def handle_info({:http, {_, :stream_end, _headers}}, s) do
+    Logger.debug("Stream End")
+    GenServer.reply(s.caller, :ok)
+    {:noreply, %{s | url: nil}}
+  end
+
+  @impl true
+  def handle_info({:http, {_ref, {{_, status_code, _}, headers, body}}}, s)
+      when status_code in @redirect_status_codes do
+    Logger.debug("Redirect")
+
+    case get_header(headers, 'location') do
+      nil ->
+        {:stop, {:http_error, {status_code, body}}, s}
+
+      next_url ->
+        if s.number_of_redirects < 5 do
+          make_request(next_url)
+          {:noreply, %{s | number_of_redirects: s.number_of_redirects + 1}}
+        else
+          {:stop, {:error, {:http_error, :too_many_redirects}}, s}
+        end
     end
+  end
 
-    args = ["--apply", "--no-unmount", "-d", devpath, "--task", "upgrade"]
+  @impl true
+  def handle_info({:http, {_ref, {{_, status_code, _}, _headers, body}}}, s) do
+    Logger.error("Error: #{status_code} #{inspect(body)}")
+    {:stop, {:error, {:http_error, {status_code, body}}}, s}
+  end
 
+  @impl true
+  def handle_info(:timeout, s) do
+    Logger.error("Error: timeout")
+    {:stop, {:error, {:http_error, :timeout}}, s}
+  end
+
+  @impl true
+  def terminate(:normal, state) do
+    GenServer.stop(state.fwup, :normal)
+    :inets.stop(:httpc, @httpc_profile)
+    :noop
+  end
+
+  @impl true
+  def terminate({:error, reason}, state) do
+    state.callback && send(state.callback, reason)
+    GenServer.stop(state.fwup, :normal)
+    :inets.stop(:httpc, @httpc_profile)
+  end
+
+  defp send_notification(nil, _what), do: :ok
+  defp send_notification(pid, what) do
+    send(pid, what)
+  end
+
+  defp fwup_args() do
+    devpath = @runtime.install_device_path()
     fwup_public_keys = NervesHub.Certificate.public_keys()
 
     if fwup_public_keys == [] do
@@ -54,44 +172,13 @@ defmodule NervesHub.HTTPFwupStream do
       Logger.error("nerves_hub won't allow this in the future.")
     end
 
-    args =
-      Enum.reduce(fwup_public_keys, args, fn public_key, args ->
-        args ++ ["--public-key", public_key]
-      end)
+    key_args =
+      Enum.reduce(fwup_public_keys, [], fn public_key, args -> ["--public-key", public_key | args] end)
 
-    {:ok, fwup} = Fwup.stream(cb, args)
-
-    {:ok,
-     %{
-       url: nil,
-       callback: cb,
-       content_length: 0,
-       buffer: "",
-       buffer_size: 0,
-       filename: "",
-       caller: nil,
-       number_of_redirects: 0,
-       timeout: 15000,
-       fwup: fwup
-     }}
+    ["--apply", "--no-unmount", "-d", devpath, "--task", "upgrade"] ++ key_args
   end
 
-  def terminate(:normal, state) do
-    GenServer.stop(state.fwup, :normal)
-    :noop
-  end
-
-  def terminate({:error, reason}, state) do
-    state.caller && GenServer.reply(state.caller, reason)
-    state.callback && send(state.callback, reason)
-    GenServer.stop(state.fwup, :normal)
-  end
-
-  def handle_call({:get, _url}, _from, %{number_of_redirects: n} = s) when n > 5 do
-    {:stop, {:error, {:http_error, :too_many_redirects}}, s}
-  end
-
-  def handle_call({:get, url}, from, s) do
+  defp make_request(url) do
     headers = [
       {'Content-Type', 'application/octet-stream'}
     ]
@@ -104,96 +191,11 @@ defmodule NervesHub.HTTPFwupStream do
       {String.to_charlist(url), headers},
       http_opts,
       opts,
-      :nerves_hub_fwup_stream
+      @httpc_profile
     )
-
-    {:noreply, %{s | url: url, caller: from}}
   end
 
-  def handle_info({:http, {_, :stream_start, headers}}, s) do
-    Logger.debug("Stream Start: #{inspect(headers)}")
-
-    content_length =
-      case Enum.find(headers, fn {key, _} -> key == 'content-length' end) do
-        nil ->
-          0
-
-        {_, content_length} ->
-          {content_length, _} =
-            content_length
-            |> to_string()
-            |> Integer.parse()
-
-          content_length
-      end
-
-    filename =
-      case Enum.find(headers, fn {key, _} -> key == 'content-disposition' end) do
-        nil ->
-          Path.basename(s.url)
-
-        {_, filename} ->
-          filename
-          |> to_string
-          |> String.split(";")
-          |> List.last()
-          |> String.trim()
-          |> String.trim("filename=")
-      end
-
-    {:noreply, %{s | content_length: content_length, filename: filename}}
-  end
-
-  def handle_info({:http, {_, :stream, data}}, s) do
-    Fwup.send_chunk(s.fwup, data)
-    {:noreply, s, s.timeout}
-  end
-
-  def handle_info({:http, {_, :stream_end, _headers}}, s) do
-    Logger.debug("Stream End")
-    GenServer.reply(s.caller, {:ok, s.buffer})
-    {:noreply, %{s | filename: "", content_length: 0, buffer: "", buffer_size: 0, url: nil}}
-  end
-
-  def handle_info({:http, {_ref, {{_, status_code, _}, headers, body}}}, s)
-      when status_code in @redirect_status_codes do
-    Logger.debug("Redirect")
-
-    case Enum.find(headers, fn {key, _} -> key == 'location' end) do
-      {'location', next_url} ->
-        handle_call({:get, List.to_string(next_url)}, s.caller, %{
-          s
-          | buffer: "",
-            buffer_size: 0,
-            number_of_redirects: s.number_of_redirects + 1
-        })
-
-      _ ->
-        {:stop, {:http_error, {status_code, body}}, s}
-    end
-  end
-
-  def handle_info({:http, {_ref, {{_, status_code, _}, _headers, body}}}, s) do
-    Logger.error("Error: #{status_code} #{inspect(body)}")
-    {:stop, {:error, {:http_error, {status_code, body}}}, s}
-  end
-
-  def handle_info(:timeout, s) do
-    Logger.error("Error: timeout")
-    {:stop, {:error, {:http_error, :timeout}}, s}
-  end
-
-  defp start_httpc() do
-    :inets.start(:httpc, profile: :nerves_hub_fwup_stream)
-
-    opts = [
-      max_sessions: 8,
-      max_keep_alive_length: 4,
-      max_pipeline_length: 4,
-      keep_alive_timeout: 120_000,
-      pipeline_timeout: 60_000
-    ]
-
-    :httpc.set_options(opts, :nerves_hub_fwup_stream)
+  def get_header(headers, key) do
+    Enum.find_value(headers, fn {k, v} -> k == key && v end)
   end
 end
