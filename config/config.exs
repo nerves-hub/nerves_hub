5cf5eb063270259b56b9b10d2be69df9e59b830a
Justin Schneck <jschneck@mac.com>
2019-02-26 11:13:49 -0800

    Move dev and test configs out of main config

--- config/config.exs
+++ config/config.exs
@@ -5,26 +5,4 @@ use Mix.Config
 # This config.exs file will configure `nerves_hub` to point to a local instance
 # of `nerves_hub_web`. See CONTRIBUTING.md for details.
 
-config :nerves_hub_cli,
-  home_dir: Path.expand("nerves-hub"),
-  ca_certs: Path.expand("../test/fixtures/ca_certs", __DIR__)
-
-# API HTTP connection.
-config :nerves_hub_user_api,
-  host: "0.0.0.0",
-  port: 4002
-
-# Device HTTP connection.
-config :nerves_hub,
-  device_api_host: "0.0.0.0",
-  device_api_port: 4001
-
-# Shared Configuration.
-config :nerves_hub,
-  ca_certs: Path.expand("../test/fixtures/ca_certs", __DIR__)
-
-config :nerves_runtime, :kernel, autoload_modules: false
-
-config :nerves_runtime, target: "host"
-
 import_config("#{Mix.env()}.exs")
