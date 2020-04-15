bdfbc00b979fadaae874fcd5ee8120a717af56ec
Justin Schneck <jschneck@mac.com>
2020-04-14 20:43:01 -0400

    Deprecate repository
    This project has been deprecated and split into two individual projects.
    Please update your project to implement one of the following two new packages.
    
    For connections using channel connections:
    [nerves_hub_link](https://github.com/nerves-hub/nerves_hub_link)
    
    For connections using HTTP polling:
    [nerves_hub_link_http](https://github.com/nerves-hub/nerves_hub_link_http)

--- config/config.exs
+++
@@ -1,8 +0,0 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
-
-# This config.exs file will configure `nerves_hub` to point to a local instance
-# of `nerves_hub_web`. See CONTRIBUTING.md for details.
-
-import_config("#{Mix.env()}.exs")
--- example/config/dev.exs
+++
@@ -1,5 +0,0 @@
-use Mix.Config
-
-# Configure shoehorn to ignore application failures in dev
-config :shoehorn,
-  handler: Shoehorn.Handler.Ignore
